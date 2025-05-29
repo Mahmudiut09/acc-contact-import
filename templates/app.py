4dd7107b10c471cc3a87fbd38fafb042a824e715
Mahmudiut09 <mahmudiut09@gmail.com>
2025-05-29 13:06:54 -0400

    Create app.py

---
+++ templates/app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, request, render_template, redirect, url_for
+import pandas as pd
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
